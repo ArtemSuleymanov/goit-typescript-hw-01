d3eaedae4358fefbdfbc9c301fa8fe06f2688d62
Artem <suleymanov2007artem@gmail.com>
2025-03-11 19:59:44 +0200

    build project and add task 1,2

---
+++ my-ts-project-1/src/basic/1.ts
@@ -0,0 +1,9 @@
+let age: number = 50;
+let userName: string = "Max";
+const toggle: boolean = true;
+const empty: null = null;
+const callback = (a: number): number => {
+  return 100 + a;
+};
+
+console.log(userName);
